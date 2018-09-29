f979596e929bf932346f9e44469192a54e92fbc4
Edward Lai <edwardjlai@gmail.com>
2018-09-29 12:44:04 -0700

    rewrite to be more efficient

--- WUDScript.js
+++ WUDScript.js
@@ -1,11 +1,4 @@
-
-function isValidDate(d) {
-  if ( Object.prototype.toString.call(d) !== "[object Date]" )
-    return false;
-  return !isNaN(d.getTime());
-}
-
-function syncCalendar() { 
+function syncCalendarScript() { 
   function convertDateToUTC(date) { return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); }
 
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("INSERT TAB NAME HERE"); // Put the tab name here
@@ -22,7 +15,7 @@ function syncCalendar() {
   var descriptionText3 = "Notes: ";
 
   var columnRange = firstColumn + ":" + lastColumn;
-  var allCells = sheet.getRange(columnRange).getValues();
+  var allCellsInWUD = sheet.getRange(columnRange).getValues();
   
   var numRows = sheet.getLastRow();
   var index = 0;
@@ -33,7 +26,7 @@ function syncCalendar() {
   var eventEnd = null;
   var eventLoc = null;
   var events = null;
-  var toCreate = [];
+  var toCreate = {};
   var matching = null;
   var firstDateValue = null;
   var missingEnd = false;
@@ -41,9 +34,10 @@ function syncCalendar() {
   var descriptionText; 
   var today = new Date();
   var threeDaysAgo = today.getTime() - 1000*60*60*24*3;
+  var key;
   
   while (index < numRows) {
-    dateOnCell = allCells[index][0];
+    dateOnCell = allCellsInWUD[index][0];
     if(isValidDate(dateOnCell)){
       var tempDate = convertDateToUTC(dateOnCell);
       if (tempDate.getTime() >= threeDaysAgo) {
@@ -55,45 +49,47 @@ function syncCalendar() {
   
   while( index < numRows)
   {
-    dateOnCell = allCells[index][0];
+    dateOnCell = allCellsInWUD[index][0];
     if(curDateValue != null || isValidDate(dateOnCell)){
       if(isValidDate(dateOnCell)){
         curDateValue = convertDateToUTC(dateOnCell);
         if (firstDateValue == null) {
           firstDateValue = curDateValue;
+          firstDateValue.setHours(0,0,0,0);
         }
       }
       
-      eventStart = allCells[index][1];
-      eventEnd = allCells[index][2];
-      eventName = allCells[index][3];      
+      eventStart = allCellsInWUD[index][1];
+      eventEnd = allCellsInWUD[index][2];
+      eventName = allCellsInWUD[index][3];      
       if(eventStart == "") {
         index++;
         continue;
       }
       if (!isValidDate(eventStart)) {
-        if (eventStart && eventStart.toString().toLowerCase().equals("all day")) {
-          eventLoc = allCells[index][4];
+        if (eventStart && (eventStart.toString().toLowerCase().equals("all day") || eventStart.toString().toLowerCase().equals("allday"))) {
+          eventLoc = allCellsInWUD[index][4];
           descriptionText = "";
           if (hasExtraColumn1) {
-            descriptionText += descriptionText1 + allCells[index][5];
+            descriptionText += descriptionText1 + allCellsInWUD[index][5];
           }
           if (hasExtraColumn2) {
-            descriptionText += "\n" + descriptionText2 + allCells[index][6];
+            descriptionText += "\n" + descriptionText2 + allCellsInWUD[index][6];
           }
           if (hasExtraColumn3) {
-            descriptionText += "\n" + descriptionText3 + allCells[index][7];
+            descriptionText += "\n" + descriptionText3 + allCellsInWUD[index][7];
           }
           eventEnd = new Date(curDateValue.getUTCFullYear(), curDateValue.getUTCMonth(), curDateValue.getUTCDate()+1, curDateValue.getHours(), 0);
-          toCreate.push({name: eventName, start: curDateValue, end: eventEnd, 
+          key = "allday;" + day + ";" + eventName;
+          toCreate[key] = {name: eventName, start: curDateValue, end: eventEnd, 
                          options: {location: eventLoc, 
-                                   description: descriptionText}, date: curDateValue, allDay: true});
+                                   description: descriptionText}, date: curDateValue, allDay: true};
           
         }
         index++;
         continue;
       }
-      if (eventEnd == "") {
+      if (eventEnd == "" || !isValidDate(eventEnd)) {
         eventEnd = new Date(eventStart);
         missingEnd = true;
       }
@@ -107,68 +103,69 @@ function syncCalendar() {
       if (eventEnd.getTime() < eventStart.getTime()) { //hack fix for when the end time is 12AM or something. Add 24 hours to move it to next day.
         eventEnd.setTime(eventEnd.getTime() + 1000*60*60*24);
       }
-      eventLoc = allCells[index][4];
+      eventLoc = allCellsInWUD[index][4];
       descriptionText = "";
       if (hasExtraColumn1) {
-        descriptionText += descriptionText1 + allCells[index][5];
+        descriptionText += descriptionText1 + allCellsInWUD[index][5];
       }
       if (hasExtraColumn2) {
-        descriptionText += "\n" + descriptionText2 + allCells[index][6];
+        descriptionText += "\n" + descriptionText2 + allCellsInWUD[index][6];
       }
       if (hasExtraColumn3) {
-        descriptionText += "\n" + descriptionText3 + allCells[index][7];
+        descriptionText += "\n" + descriptionText3 + allCellsInWUD[index][7];
       }
-      toCreate.push({name: eventName, start: eventStart, end: eventEnd, 
+      key = "" + eventStart.getTime() + ";" + eventEnd.getTime() + ";" + eventName;
+      toCreate[key] = {name: eventName, start: eventStart, end: eventEnd, 
                      options: {location: eventLoc, 
-                               description: descriptionText}, date: curDateValue, allDay: false});
+                               description: descriptionText}, date: curDateValue, allDay: false};
     }
     index++;
   }
 //  Logger.log(toCreate);
 //  Logger.log(".................");
-  while (firstDateValue.getTime() <= curDateValue.getTime() && toCreate.length > 0) {
+  while (firstDateValue.getTime() <= curDateValue.getTime()) {
     events = myCalendar.getEventsForDay(firstDateValue);
     for(var e in events){
       matching = null;
-      for (var item in toCreate) {
-        if (events[e].isAllDayEvent() && toCreate[item]['allDay'] == true) {
-          if (toCreate[item]['date'].getTime() == firstDateValue.getTime() 
-            && toCreate[item]['name'] == events[e].getTitle() 
-            && events[e].getLocation() == toCreate[item]['options']['location']
-            && events[e].getDescription() == toCreate[item]['options']['description']) {
-              matching = toCreate[item];
-              break;
-            }
-        }
-        if (toCreate[item]['date'].getTime() == firstDateValue.getTime() 
-          && toCreate[item]['name'] == events[e].getTitle()
-          && events[e].getStartTime().valueOf() == toCreate[item]['start'].valueOf()
-          && events[e].getEndTime().valueOf() == toCreate[item]['end'].valueOf()
-          && events[e].getLocation() == toCreate[item]['options']['location']
-          && events[e].getDescription() == toCreate[item]['options']['description']) {
-          matching = toCreate[item];
-          break;
-        }
-      }
-      if (matching == null && (events[e].getStartTime().valueOf() >= firstDateValue.getTime().valueOf() || events[e].isAllDayEvent())) { //calendar event not found, deleting 
+      if (events[e].isAllDayEvent())
+        key = "allday;" + events[e].getAllDayStartDate().getDate() + ";" + events[e].getTitle();
+      else
+        key = "" + events[e].getStartTime().getTime() + ";" + events[e].getEndTime().getTime() + ";" + events[e].getTitle();
+      if (!(key in toCreate) && (events[e].getStartTime().valueOf() >= firstDateValue.getTime().valueOf() || events[e].isAllDayEvent())) {
+        // can update who's in charge, location, and who's involved if needed, then pop the event
         toDelete[events[e].getId()] = events[e];
-      } else { //calendar event found, don't create (i.e. remove from create list)
-        toCreate.splice(toCreate.indexOf(matching), 1);
+      } else {
+        if (events[e].getLocation() != toCreate[key]['options']['location'] || events[e].getDescription() != toCreate[key]['options']['description']) {
+          events[e].setLocation(toCreate[key]['options']['location']);
+          events[e].setDescription(toCreate[key]['options']['description']);
+        }
+        delete toCreate[key];
       }
+
     }
     firstDateValue.setDate(firstDateValue.getUTCDate() + 1);
   }
 //  Logger.log(toDelete);
 //  Logger.log(toCreate);
+  var counter = 0;
   for (var e in toCreate) {
     if (toCreate[e]['allDay']) {
       myCalendar.createAllDayEvent(toCreate[e]['name'], toCreate[e]['date'], toCreate[e]['options']);
+      counter++;
+      if (counter % 20 == 0)
+        Utilities.sleep(1000);
     } else {
       myCalendar.createEvent(toCreate[e]['name'], toCreate[e]['start'], toCreate[e]['end'], toCreate[e]['options']);
+      counter++;
+      if (counter % 20 == 0)
+        Utilities.sleep(1000);
     }
   }
   for(var key in toDelete){
     toDelete[key].deleteEvent();
+    counter++;
+    if (counter % 20 == 0)
+      Utilities.sleep(1000);
   }
 }
 
